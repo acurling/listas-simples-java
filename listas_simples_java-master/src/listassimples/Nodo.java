d6a3ea42c7d0e8b5402cc60a28c705ab7ef19aff
acurling <acurling@hotmail.com>
2023-03-16 20:40:57 -0600

    Lista simple

---
+++ listas_simples_java-master/src/listassimples/Nodo.java
@@ -0,0 +1,43 @@
+package listassimples;
+/**
+ * Clase que define los elementos que debe tener un Nodo de la lista.
+ * @author xavier
+ */
+public class Nodo {
+    // Variable en la cual se va a guardar el valor.
+    private int valor;
+    private String nombre;
+    private String cedula;
+
+
+
+    // Variable para enlazar los nodos.
+    private Nodo siguiente;
+    private Nodo Anterior;
+ 
+    /**
+     * Constructor que inicializamos el valor de las variables.
+     */
+    public void Nodo(){
+        this.valor = 0;
+        this.siguiente = null;
+    }
+    
+    // Métodos get y set para los atributos.
+    
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }   
+}
